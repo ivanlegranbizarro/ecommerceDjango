8e59569e077e4750944c3eb83f7544ce3707614e
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2021-09-14 06:43:03 +0200

    Implementar lógica variaciones productos

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, Variation
 
 # Register your models here.
 
@@ -10,4 +10,13 @@ class ProductoAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug_producto': ['nombre_producto', ]}
 
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('producto', 'categoria_variacion',
+                    'is_active', 'valores_variacion')
+    list_editable = ('is_active',)
+    list_filter = ('producto', 'is_active',
+                   'valores_variacion', 'categoria_variacion')
+
+
 admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Variation, VariationAdmin)
--- store/models.py
+++ store/models.py
@@ -22,3 +22,20 @@ class Producto(models.Model):
 
     def __str__(self):
         return self.nombre_producto
+
+
+class Variation(models.Model):
+
+    variaciones = (
+        ('color', 'Color'),
+        ('talla', 'Talla'),
+    )
+
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    categoria_variacion = models.CharField(max_length=10, choices=variaciones)
+    valores_variacion = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.producto.nombre_producto
